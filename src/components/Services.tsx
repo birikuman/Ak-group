8f0f171ae60363dfbb2f2228101fc03c543cd5bd
lewis BIRIKUMANA <birilewis2019@gmail.com>
2026-09-14 04:45:45 +0200

    Update Branding & Printing service image to print.jpg

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -6,7 +6,7 @@ const SERVICES_DATA = [
     id: 'branding-printing',
     title: 'Branding & Printing',
     tagline: 'Screen Printing, 3D Embroidery, Engraving & Foil Stamping',
-    image: '/images/service-branding.jpg'
+    image: '/images/print.jpg'
   },
   {
     id: 'corporate-merchandise',
